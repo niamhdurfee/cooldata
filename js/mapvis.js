0c04c53915981a9f217838bf7d2f40a77faf2113
Dayne Davis <daynedavis5@gmail.com>
2015-04-09 22:29:36 -0400

    cool lines

--- js/mapvis.js
+++ js/mapvis.js
@@ -1,5 +1,5 @@
 /**
- * MapVis 
+ * MapVis
  * @param _parentElement -- the HTML or SVG element (D3 node) to which to attach the vis
  * @param _data -- the data array
  * @param _metaData -- the meta-data / data description object
@@ -30,7 +30,11 @@ MapVis = function(_parentElement, _data, _metaData) {
 MapVis.prototype.initVis = function() {
 
   var map = L.mapbox.map('mapVis', 'niamhdurfee.lmdl8410');
-
+  var polyline_options = {
+    color: '#222'
+  };
+  var line_points = [[42.361285,-71.06514],[42.353412,-71.044624]]
+  var polyline = L.polyline(line_points, polyline_options).addTo(map);
 
 
   // // filter, aggregate, modify data
@@ -55,7 +59,7 @@ MapVis.prototype.updateVis = function() {
  * @param selection
  */
 MapVis.prototype.onSelectionChange = function() {
-  
+
   this.updateVis();
 }
 
