a5247cbc3d2c943bf0917233014f90fe0796697a
niamhdurfee <durfee@college.harvard.edu>
2015-04-10 12:35:29 -0400

    updated to include speed and distance

--- data/scripts/split_gender.py
+++ data/scripts/split_gender.py
@@ -4,12 +4,10 @@ import os
 trips =  open('../trips.min.csv','r').readlines()
 
 trips = [re.split(",",trip[:-2]) for trip in trips]
-header = ['duration', 'start_date', 'strt_statn', 'end_statn', 'zip_code', 'age','ID']
+header = ['duration', 'start_date', 'strt_statn', 'end_statn', 'zip_code', 'age','ID','dist','speed']
 
 pathMale = 'trips_male.csv'
 pathFemale = "trips_female.csv"
-os.remove(pathMale)
-os.remove(pathFemale)
 
 male =  csv.writer(open(pathMale,'a'))
 female =  csv.writer(open(pathFemale,'a'))
@@ -19,7 +17,7 @@ female.writerow(header)
 for trip in trips[1:]:
 	if trip[4] =='R':
 		trip.remove('R')
-		g = trip.pop(-2)
+		g = trip.pop(-4)
 		if g == 'M':
 			male.writerow(trip)
 		else:
--- data/scripts/split_registration.py
+++ data/scripts/split_registration.py
@@ -2,16 +2,14 @@ import csv
 import re
 import os
 trips =  open('../trips.min.csv','r').readlines()
-
 trips = [re.split(",",trip[:-2]) for trip in trips]
 
-headCasual=['duration', 'start_date', 'strt_statn', 'end_statn', 'ID']
-headRegistered = ['duration', 'start_date', 'strt_statn', 'end_statn', 'zip_code', 'age', 'gender', 'ID']
+headCasual=['duration', 'start_date', 'strt_statn', 'end_statn', 'ID','dist','speed']
+headRegistered = ['duration', 'start_date', 'strt_statn', 'end_statn', 'zip_code', 'age', 'gender', 'ID','dist','speed']
 
 pathCasual = 'trips_casual.csv'
 pathReg = "trips_registered.csv"
-os.remove(pathCasual)
-os.remove(pathReg)
+
 
 casual =  csv.writer(open(pathCasual,'a'))
 reg =  csv.writer(open(pathReg,'a'))
@@ -23,6 +21,5 @@ for trip in trips[1:]:
 		trip.remove('R')
 		reg.writerow(trip)
 	else:
-		row = trip[:4]
-		row.append(trip[-1])
+		row = trip[:4] + trip[-3:]
 		casual.writerow(row)
