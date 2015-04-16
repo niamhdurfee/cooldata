dbb83989c4f2cb2a1def0a8d4cce09008f16a10b
niamhdurfee <durfee@college.harvard.edu>
2015-04-16 14:56:03 -0400

    DAYNE THIS IS FOR YOU

--- data/scripts/station.py
+++ data/scripts/station.py
@@ -1,30 +1,18 @@
 import re,os,csv
 
-lines = [re.sub('\r\n','',line) for line in open('../stations.csv')]
-lines = [re.split(',',line) for line in lines[1:]]
-num=[]
-for station in lines:
-	num.append(int(station[0]))
-num.sort()
-
-routes = dict()
-for n in num:
-	routes[n] = dict()
-
-for n in num:
-	for a in num:
-		if (a < n):
-			routes[a][n] = dict() 
-		if (a >n):
-			routes[n][a] = dict()
-i = 0
-datafile = csv.writer(open('../possibletrips.csv','a'))
-datafile.writerow(['ID','orig','dest'])
-for route in routes:
-	for end in routes[route]:
-		i +=1
-		datafile.writerow([i,route,end])
-
-
-
-		
+lines = [re.sub("\r\n","",line) for line in open("../stations.csv")]
+lines = [re.split(",",line) for line in lines[1:]]
+#print lines
+path ="stations.json"
+os.remove(path)
+f = open(path,"a")
+f.write('{')
+for route in lines:
+	f.write('"'+route[0]+'": \n{')
+	f.write('"fullname":"'+route[1]+'",\n')
+	f.write('"short":"'+route[2]+'",\n')
+	f.write('"lat":'+route[3]+',\n')
+	f.write('"long":'+route[4]+',\n')
+	f.write('"hood":"'+route[5]+'" \n },\n')
+f.write("}")
+f.close()		
