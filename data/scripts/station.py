9c8292e9233a3e170b66890d8d992508ff070e42
niamhdurfee <durfee@college.harvard.edu>
2015-04-16 21:04:20 -0400

    this one goes out to Dayne. the file is now 26x bigger :)

--- data/scripts/station.py
+++ data/scripts/station.py
@@ -2,17 +2,16 @@ import re,os,csv
 
 lines = [re.sub("\r\n","",line) for line in open("../stations.csv")]
 lines = [re.split(",",line) for line in lines[1:]]
-#print lines
 path ="stations.json"
 os.remove(path)
 f = open(path,"a")
 f.write('{')
-for route in lines:
-	f.write('"'+route[0]+'": \n{')
+for station in stations:
+	f.write('"'+station[0]+'": \n{')
 	f.write('"fullname":"'+route[1]+'",\n')
-	f.write('"short":"'+route[2]+'",\n')
-	f.write('"lat":'+route[3]+',\n')
-	f.write('"long":'+route[4]+',\n')
-	f.write('"hood":"'+route[5]+'" \n },\n')
+	f.write('"short":"'+station[2]+'",\n')
+	f.write('"lat":'+station[3]+',\n')
+	f.write('"long":'+station[4]+',\n')
+	f.write('"hood":"'+station[5]+'" \n },\n')
 f.write("}")
 f.close()		
