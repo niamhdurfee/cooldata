52213bffedaf3f7020f981746f522c45fea1ac84
Alexandra Smith <alexandrasmith@college.harvard.edu>
2015-04-21 10:03:30 -0400

    added labels

--- js/chordvis.js
+++ js/chordvis.js
@@ -35,28 +35,28 @@ ChordVis.prototype.initVis = function() {
     var that = this;
 
 
-this.arc = d3.svg.arc()
-.innerRadius(this.innerRadius)
-.outerRadius(this.outerRadius);
- 
-this.layout = d3.layout.chord()
-.padding(.04)
-.sortSubgroups(d3.descending)
-.sortChords(d3.ascending);
- 
-this.path = d3.svg.chord()
-.radius(this.innerRadius);
- 
-this.svg = this.parentElement.append("svg")
-.attr("width", this.width)
-.attr("height", this.height)
-.append("g")
-.attr("id", "circle").attr("fill","none")
-.attr("transform", "translate(" + (this.width / 2) + "," + (this.height / 2) + ")");
- 
-this.svg.append("circle")
-.attr("r", this.outerRadius);
-    
+    this.arc = d3.svg.arc()
+    .innerRadius(this.innerRadius)
+    .outerRadius(this.outerRadius);
+
+    this.layout = d3.layout.chord()
+    .padding(.04)
+    .sortSubgroups(d3.descending)
+    .sortChords(d3.ascending);
+
+    this.path = d3.svg.chord()
+    .radius(this.innerRadius);
+
+    this.svg = this.parentElement.append("svg")
+    .attr("width", this.width)
+    .attr("height", this.height)
+    .append("g")
+    .attr("id", "circle").attr("fill","none")
+    .attr("transform", "translate(" + (this.width / 2) + "," + (this.height / 2) + ")");
+
+    this.svg.append("circle")
+    .attr("r", this.outerRadius);
+
     // filter, aggregate, modify data
     // this.wrangleData();
 
@@ -73,76 +73,85 @@ ChordVis.prototype.wrangleData = function(_filterFunction) {
 }
 
 ChordVis.prototype.updateVis = function() {
-  var that = this;
-  var color = d3.scale.category20();
+    var that = this;
+    var color = d3.scale.category20();
 
-var formatPercent = d3.format(".1%");
+    var formatPercent = d3.format(".1%");
+
+
+    // Compute the chord layout.
+    this.layout.matrix(_matrixData);
+
+    // Add a group per neighborhood.
+    var group = this.svg.selectAll(".group")
+    .data(this.layout.groups)
+    .enter().append("g")
+    .attr("class", "group")
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout);
+
+    // Add a mouseover title.
+    group.append("title").text(function(d, i) {
+        return that.neighborhoods[i].name + ": " + formatPercent(d.value) + " of origins";
+    });
+    
+    group.append("svg:text")
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { console.log(d); return ((d.startAngle + d.endAngle) / 2) > Math.PI ? "end" : null; })
+        .attr("transform", function(d) {
+        return "rotate(" + (((d.startAngle + d.endAngle) / 2) * 180 / Math.PI - 90) + ")"
+            + "translate(" + that.height/2. + ( - 15) + ")"
+            + (((d.startAngle + d.endAngle) / 2) > Math.PI ? "rotate(180)" : "");
+        })
+        .text(function(d,i) {
+            return that.neighborhoods[i].name;
+        })
+        .style("font-size","10px")
+        .style("fill", "black")
+        .style("z-index", "10000")
+
+
+    
+    
+    // Add the group arc.
+    var groupPath = group.append("path")
+    .attr("id", function(d, i) { return "group" + i; })
+    .attr("d", this.arc)
+    .style("fill", function(d, i) { return that.neighborhoods[i].color; });
+
+    // Add the chords.
+    var chord = this.svg.selectAll(".chord")
+    .data(this.layout.chords)
+    .enter().append("path")
+    .attr("class", "chord")
+    .style("fill", function(d) { return that.neighborhoods[d.source.index].color; })
+    .attr("d", this.path);
+
+    // Add an elaborate mouseover title for each chord.
+     chord.append("title").text(function(d) {
+     return that.neighborhoods[d.source.index].name
+     + " → " + that.neighborhoods[d.target.index].name
+     + ": " + formatPercent(d.source.value)
+     + "\n" + that.neighborhoods[d.target.index].name
+     + " → " + that.neighborhoods[d.source.index].name
+     + ": " + formatPercent(d.target.value);
+     });
+    
+    function mouseover(d, i) {
+        chord.classed("fade", function(p) {
+        return p.source.index != i
+        && p.target.index != i;
+        });
+    }
+    function mouseout(d, i) {
+        chord.classed("fade", function(p) { return 0});
+    }
  
- 
-// Compute the chord layout.
-this.layout.matrix(_matrixData);
- 
-// Add a group per neighborhood.
-var group = this.svg.selectAll(".group")
-.data(this.layout.groups)
-.enter().append("g")
-.attr("class", "group")
-.on("mouseover", mouseover)
-.on("mouseout", mouseout);
- 
-// Add a mouseover title.
-group.append("title").text(function(d, i) {
-return that.neighborhoods[i].name + ": " + formatPercent(d.value) + " of origins";
-});
- 
-// Add the group arc.
-var groupPath = group.append("path")
-.attr("id", function(d, i) { return "group" + i; })
-.attr("d", this.arc)
-.style("fill", function(d, i) { return that.neighborhoods[i].color; });
- 
-// Add a text label.
-var groupText = group.append("text")
-.attr("x", 6)
-.attr("dy", 15);
- 
-groupText.append("textPath")
-.attr("xlink:href", function(d, i) { return "#group" + i; })
-.text(function(d, i) { return that.neighborhoods[i].name; });
- 
-// Remove the labels that don't fit. :(
-groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength(); })
-.remove();
- 
-// Add the chords.
-var chord = this.svg.selectAll(".chord")
-.data(this.layout.chords)
-.enter().append("path")
-.attr("class", "chord")
-.style("fill", function(d) { return that.neighborhoods[d.source.index].color; })
-.attr("d", this.path);
- 
-// Add an elaborate mouseover title for each chord.
- chord.append("title").text(function(d) {
- return that.neighborhoods[d.source.index].name
- + " → " + that.neighborhoods[d.target.index].name
- + ": " + formatPercent(d.source.value)
- + "\n" + that.neighborhoods[d.target.index].name
- + " → " + that.neighborhoods[d.source.index].name
- + ": " + formatPercent(d.target.value);
- });
- 
-function mouseover(d, i) {
-chord.classed("fade", function(p) {
-return p.source.index != i
-&& p.target.index != i;
-});
-}
-function mouseout(d, i) {
-chord.classed("fade", function(p) { return 0});
 }
 
-}
+
+
+
 
 /**
  * Gets called by event handler and should create new aggregated data
