c2746c89d27af2350b182765a05fc13197225339
niamhdurfee <durfee@college.harvard.edu>
2015-04-22 12:50:40 -0400

    added hourly stats for gender/registration/user type

--- data/scripts/getRoutes.py
+++ data/scripts/getRoutes.py
@@ -5,13 +5,14 @@ temp = [re.split(',',station[:-2]) for station in temp[1:]]
 stations = dict()
 for station in temp:
 	stations[station[0]] = station[1:]
-trips = [re.split(',', trip[:-2]) for trip in open('../possibletrips.csv')]
+trips = [re.split(',', trip[:-2]) for trip in open('../routes.csv')]
 trips = trips[1:]
-trips = trips[:1]
-i = 1
-workingTrips = []
-notworking = []
-f = open('temp.txt','w')
+trips = trips[4721:]
+path = 'temp1.csv'
+os.remove(path)
+f = csv.writer(open(path,'a'))
+header = ['ID','orig','dest','polyline']
+f.writerow(header)
 for trip in trips:
 	orig = stations[str(trip[1])]
 	dest = stations[str(trip[2])]
@@ -19,27 +20,12 @@ for trip in trips:
 	dest_coord = float(dest[-3]), float(dest[-2])
 	url = "http://maps.googleapis.com/maps/api/directions/json?origin={0}&destination={1}&mode=bicycling".format(str(orig_coord),str(dest_coord))
 	result= simplejson.load(urllib.urlopen(url))
-	f.write(str(result))
-#	if result['status'] == 'OVER_QUERY_LIMIT':
-#		print "INDEX OF BUST "+str(i)
-#		break
-#	res = result['rows'][0]['elements'][0]
-#	if res['status'] != 'OK':
-#		trip.append(i)
-#		trip.append(res['status'])
-#		notworking.append(trip)
-#	else:
-#		row = [i, trip[0], trip[1],res['duration']['value'], res['distance']['value']]
-#		workingTrips.append(row)
-#	print i, res['status']
-#	time.sleep(.1)
-#	i += 1
-#
-#datafile = csv.writer(open("trips_statsAllison.csv",'a'))
-#nwfile = csv.writer(open('notworkingAllison.csv','a'))
-#
-#header = ['ID','orig','dest','duration','distance']
-#nwfile.writerows(notworking)
-#datafile.writerow(header)
-#datafile.writerows(workingTrips)
+	if result['status'] == 'OVER_QUERY_LIMIT':
+		print "OVER LIMIT AT INDEX "+str(trip[0])
+		break
+	res = result['routes'][0]['overview_polyline']['points']
+	trip.append(res)
+	f.writerow(trip)
+	print trip[0], result['status']
+	time.sleep(0.35)
 
