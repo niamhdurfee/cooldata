081fa03e171a49d883f26e305ee46adb8feb477e
Dayne Davis <daynedavis5@gmail.com>
2015-05-03 16:20:31 -0400

    linegraph for whenVis

--- js/whenvis.js
+++ js/whenvis.js
@@ -16,8 +16,8 @@ WhenVis = function(_parentElement, _data, _eventHandler) {
   this.margin = {
       top: 10,
       right: 10,
-      bottom: 30,
-      left: 30
+      bottom: 60,
+      left: 45
     },
   this.width = this.parentElement.node().clientWidth - this.margin.left - this.margin.right,
   this.height =  this.parentElement.node().clientHeight- this.margin.top - this.margin.bottom;
@@ -28,12 +28,17 @@ WhenVis = function(_parentElement, _data, _eventHandler) {
 /**
  * Method that sets up the SVG and the variables
  */
+ var formatCount = d3.format(",.0f"),
+    formatTime = d3.time.format("%H:%M"),
+    formatMinutes = function(d) { return formatTime(new Date(2012, 0, 1, 0, d)); };
+
 WhenVis.prototype.initVis = function() {
   var that = this;
 
   this.color = d3.scale.ordinal().domain(colorDomain).range(colorRange);
 
-  this.x = d3.time.scale()
+  this.x = d3.scale.linear()
+  .domain([0,1440])
     .range([this.margin.left, this.width]);
 
   this.y = d3.scale.linear()
@@ -42,6 +47,7 @@ WhenVis.prototype.initVis = function() {
 
   this.xAxis = d3.svg.axis()
     .scale(this.x)
+    .tickFormat(formatMinutes)
     .orient("bottom");
 
   this.yAxis = d3.svg.axis()
@@ -58,6 +64,11 @@ WhenVis.prototype.initVis = function() {
   	.interpolate("basis")
     .x(function(d) { return that.x(d.date); })
     .y(function(d) { return that.y(d.value); })
+
+  this.weekendLine = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d) { return that.x(d.date); })
+    .y(function(d) { return that.y(d.weekend); })
 
   this.svg = this.parentElement.append("svg")
     .attr("width", this.width + this.margin.left + this.margin.right)
@@ -90,8 +101,8 @@ WhenVis.prototype.updateVis = function() {
 
   var that = this;
 
-  this.x.domain(d3.extent(that.displayData[0].values, function(d) { return d.date; }));
-  this.y.domain([0,d3.max(that.displayData, function (d) { return d3.max(d.values, function (a) {return a.value})})]);
+  // this.x.domain(d3.extent(that.displayData, function(d) { console.log(d); return d.timeofday; }));
+  this.y.domain([0,d3.max(that.displayData, function (d) { return d3.max(d.total, function (a) {return a.value})})]);
 
   this.svg.select(".y.axis")
     .call(this.yAxis);
@@ -105,16 +116,18 @@ WhenVis.prototype.updateVis = function() {
   user.enter().append("g")
       .attr("class", "user")
       .append("path")
-      .attr("class", "area");
 
-  // user.select('.area').attr("d", function(d) { return that.area(d.values); })
-  //     .transition()
-  //     .style("fill", function(d) { return that.color(d.type); })
-  //     .style("opacity", 0.5);
+  this.svg
+      .append("text")
+      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+      .attr("transform", "translate("+this.width/2+","+(this.height+50)+")")  // text is drawn off the screen top left, move down and out and rotate
+      .text("Time of Day");
+  this.svg
+      .append("text")
+      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+      .attr("transform", "translate("+this.margin.left/4+","+(this.height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+      .text("Average Speed (mph)");
 
-  // user.select('.area')
-  //     .on("mouseover", this.mouseover)
-  // 	  .on("mouseout", this.mouseout);
 
   var line = this.svg.selectAll(".userline")
   	  .data(this.displayData);
@@ -125,11 +138,38 @@ WhenVis.prototype.updateVis = function() {
 	  .attr("class","line");
 
   line.select(".line")
-  	  .attr("d",function (d) { return that.line(d.values)})
+  	  .attr("d",function (d) { return that.line(d.total)})
   	  .style("stroke", function (d) {return that.color(d.type)});
+
+  var weekendLine = this.svg.selectAll(".weekendLine")
+  	  .data(this.displayData);
+
+  weekendLine.enter().append("g")
+  	  .attr("class","weekendLine")
+  	  .append("path")
+	  .attr("class","line");
+
+  weekendLine.select(".line")
+  	  .attr("d",function (d) { return that.line(d.weekend)})
+  	  .style("stroke", 'red');
+
+  var weekdayLine = this.svg.selectAll(".weekdayLine")
+      .data(this.displayData);
+
+  weekdayLine.enter().append("g")
+      .attr("class","weekdayLine")
+      .append("path")
+    .attr("class","line");
+
+  weekdayLine.select(".line")
+      .attr("d",function (d) { return that.line(d.weekday)})
+      .style("stroke", 'blue');
+
 
   user.exit().remove();
   line.exit().remove();
+  weekendLine.exit().remove();
+  weekdayLine.exit().remove();
 }
 
 /**
@@ -165,25 +205,41 @@ WhenVis.prototype.onTypeChange = function(_dom) {
  * */
 
 WhenVis.prototype.filterAndAggregate = function(_filter) {
-  // Set filter to a function that accepts all items
-  var filter = function() {
-    return true;
-  }
-  if (_filter != null) {
-    this.filter = _filter;
-  } else {
-  	this.filter = filter;
-  }
+
+  var makeMinutes = function(s) {
+    a = s.split(":");
+    var minutes = 0;
+    minutes += parseInt(a[0]) * 60;
+    minutes += parseInt(a[1]);
+    return minutes
+  };
+
+  var makeValue = function(dist, time) {
+    return parseFloat(dist.replace(/,/g, ''))/time*60;
+  };
+
+  var makeDifValue = function(totald, weekendd, totalt, weekendt) {
+    return (parseFloat(totald.replace(/,/g, '')) - parseFloat(weekendd.replace(/,/g, '')))/(totalt -weekendt)*60;
+  };
+
   var that = this;
-  var res = this.data.filter(this.filter);
+  var res = this.data;
   res = that.dom.map(function (t) {
     return {
       type: t,
-      values: res.map(function (d) {
-        return {date: d.date, value: d[t]}
+      total: res.map(function (d) {
+        return {date: makeMinutes(d.timeofday),value: makeValue(d.dist,d.time)};
+      }),
+      weekend: res.map(function (d) {
+        return {date: makeMinutes(d.timeofday),value: makeValue(d.distWeekend, d.timeWeekend)};
+      }),
+      weekday: res.map(function (d) {
+        return {date: makeMinutes(d.timeofday),value: makeDifValue(d.dist, d.distWeekend, d.time, d.timeWeekend)};
       })
-    }
+    };
   });
+
+  console.log(res);
   return res;
 }
 
