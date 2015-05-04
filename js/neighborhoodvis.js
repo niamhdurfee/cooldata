919e26fa29e99c14ff3ddb61fe141bdb9a8a5ba8
niamhdurfee <durfee@college.harvard.edu>
2015-05-04 18:09:32 -0400

    removed console.log() states

--- js/neighborhoodvis.js
+++ js/neighborhoodvis.js
@@ -1,15 +1,17 @@
 /**
- * FollowVis
+ * NeighborhoodVis
  * @param _parentElement -- the HTML or SVG element (D3 node) to which to attach the vis
  * @param _data -- the data array
  * @param _metaData -- the meta-data / data description object
  * @constructor
  */
-FollowVis = function(_parentElement, _metaData,_eventHandler) {
+NeighborhoodVis = function(_parentElement, _data, _metaData,_eventHandler) {
   this.parentElement = _parentElement;
-  this.routeData = _metaData;
+  this.stationData = _data;
+  this.neighborhoods = _metaData;
   this.eventHandler = _eventHandler;
   this.displayData = [];
+  this.matrix = _matrixData;
 
   // Define all "constants" here
   this.margin = {
@@ -27,29 +29,20 @@ FollowVis = function(_parentElement, _metaData,_eventHandler) {
 /**
  * Method that sets up the SVG and the variables
  */
-FollowVis.prototype.initVis = function() {
-  var that = this;
-  this.map = new L.mapbox.map('map', 'niamhdurfee.m3dcii07',{center: [42.35272,-71.09], zoom: 13});
+NeighborhoodVis.prototype.initVis = function() {
 
-  d3.entries(this.routeData).forEach( function (d) {L.Polyline.fromEncoded(d.value.polyline, {stroke:5,opacity:0,className:"trips od"+d.key}).addTo(that.map)})
+  // // filter, aggregate, modify data
+  this.wrangleData(null);
+
   // // call the update method
   this.updateVis();
 }
 
-FollowVis.prototype.tick = function(i,trip) {
-  if (i == 0) { 
-    d3.selectAll(".trips").attr("stroke-opacity",0).classed("taken",false);
+NeighborhoodVis.prototype.wrangleData = function(_filterFunction) {
+  this.displayData = this.filterAndAggregate(_filterFunction);
+}
 
-  }
-
-  var a = ".od" + trip.origdest;
-  d3.selectAll(".taken").attr("stroke-opacity",0.1).attr("stroke-width",5);
-  d3.select(a).attr("stroke-opacity",1).attr("stroke-width",8).attr("stroke",trip.tripType).classed("taken",true);
-
-  }
- 
-
-FollowVis.prototype.updateVis = function() {
+NeighborhoodVis.prototype.updateVis = function() {
   var that = this;
 }
 
@@ -59,7 +52,7 @@ FollowVis.prototype.updateVis = function() {
  * be defined here.
  * @param selection
  */
-FollowVis.prototype.onSelectionChange = function() {
+NeighborhoodVis.prototype.onSelectionChange = function() {
 
   this.updateVis();
 }
@@ -72,7 +65,7 @@ FollowVis.prototype.onSelectionChange = function() {
  *
  * */
 
-FollowVis.prototype.filterAndAggregate = function(_filter) {
+NeighborhoodVis.prototype.filterAndAggregate = function(_filter) {
   // Set filter to a function that accepts all items
   var filter = function() {
     return true;
@@ -83,5 +76,5 @@ FollowVis.prototype.filterAndAggregate = function(_filter) {
 
   var that = this;
 
-  return 0;
+  return res;
 }
